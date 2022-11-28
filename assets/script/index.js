618d01c62082eddb953abe954ffc0b928af8ae3b
Shaun <117479605+LamingerShaun@users.noreply.github.com>
2022-11-28 17:28:30 -0600

    working on the modal 2

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let modal = document.querySelector('.modal'),
+openModal = document.querySelector('.user'),
+closeModal = document.querySelector('.close-modal');
+
+openModal.addEventListener('click', function() {
+    modal.style.display = 'block';
+})
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+})
+
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+})
